c9634fbd6f2869d89e47a9c0c8071e54033b7619
Roy Wati Munialo <rwati@hq.ekenya.co.ke>
2020-05-20 22:12:40 +0300

    initial commit

---
+++ src/test/java/ekenya/co/ke/dbapiloadbalancer/DbApiLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package ekenya.co.ke.dbapiloadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApiLoadBalancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
